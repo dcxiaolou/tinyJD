1932661336a31cf589ac0a64560d3d4a98e3d287
dcxiaolou <3515668588@qq.com>
2019-10-17 00:37:29 +0800

    service的工具类-ActiveMQ、Redis

---
+++ tinyjd-service-util/src/main/java/com/dcxiaolou/tinyJD/mq/ActiveMQConfig.java
@@ -0,0 +1,53 @@
+package com.dcxiaolou.tinyJD.mq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+@Configuration
+public class ActiveMQConfig {
+    @Value("${spring.activemq.broker-url:disabled}")
+    String brokerURL;
+    @Value("${activemq.listener.enable:disabled}")
+    String listenerEnable;
+
+    @Bean
+    public ActiveMQUtil getActiveMQUtil() throws JMSException {
+        if (brokerURL.equals("disabled")) {
+            return null;
+        }
+        ActiveMQUtil activeMQUtil = new ActiveMQUtil();
+        activeMQUtil.init(brokerURL);
+        return activeMQUtil;
+    }
+
+    //定义一个消息监听器连接工厂， 这里定义的是点对点模式的监听器连接工厂
+    @Bean(name = "jmsQueueListener")
+    public DefaultJmsListenerContainerFactory jmsQueueListenerContainerFactory(ActiveMQConnectionFactory activeMQConnectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new
+                DefaultJmsListenerContainerFactory();
+        if (!listenerEnable.equals("true")) {
+            return null;
+        }
+        //将mq的监听器封装到springboot的容器中
+        factory.setConnectionFactory(activeMQConnectionFactory);
+        //设置并发数
+        factory.setConcurrency("5");
+        factory.setRecoveryInterval(5000L);
+        factory.setSessionTransacted(false);
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+        return factory;
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory activeMQConnectionFactory =
+                new ActiveMQConnectionFactory(brokerURL);
+        return activeMQConnectionFactory;
+    }
+}
